82ed63c5acb2e6bacbabe9ffeb3d021d511cae76
Tai Smith <imadeanomelet@gmail.com>
2021-03-21 13:10:57 -0500

    Initial commit; we got license, read me

---
+++ src/main/java/org/launchcode/questionquestin/QuestionQuestinApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.questionquestin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionQuestinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionQuestinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/questionquestin/QuestionQuestinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.questionquestin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionQuestinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
